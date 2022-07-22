f25ea43f2d9654a4933cb47cefc51c22530fbb18
Pierre Le Roux <leroux.pierre@gmail.com>
2022-07-22 22:55:21 +0200

    day-2: Completed

---
+++ Day 2/billcalculator.py
@@ -0,0 +1,5 @@
+print('Welcome to the tip calculator.')
+total_bill = int(input('What was the total bill? $'))
+number_of_people = int(input('How many people to split the bill? '))
+percentage_tip = int(input('What percentage tip would you like to give? 10, 12, or 15? '))
+print(f'Each person should pay: ${total_bill*(1 + (percentage_tip/100))/number_of_people}')
