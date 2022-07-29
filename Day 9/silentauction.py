4a802378bc9d7bab3047612ad0438888509c6025
Pierre Le Roux <leroux.pierre@gmail.com>
2022-07-29 23:28:35 +0200

    Day-9: Gavel ascii art, bid and max_bid functions

--- Day 9/silentauction.py
+++ Day 9/silentauction.py
@@ -3,3 +3,27 @@
 # How much would you like to bid? R
 # Are there any other bidders?
 # Show the bidder name with the highest bid
+
+gavel = """
+     ___________
+     \         /
+      )_______(
+      |"""""""|_.-._,.---------.,_.-._
+      |       | | |               | | ''-.
+      |       |_| |_             _| |_..-'
+      |_______| '-' `'---------'` '-'
+      )"""""""(
+     /_________\
+     `'-------'`
+   .-------------.
+  /_______________\
+"""
+
+def bid():
+    name = input('What is your name? ')
+    bid_amount = input('How much would you like to bid? R')
+    
+    return name, bid_amount
+
+def max_bid(bids):
+    return max(bids, key=bids.get)
