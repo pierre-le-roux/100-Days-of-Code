c157cc2b5c5da8c5832d80dd95c2ee849e7d185e
Pierre Le Roux <leroux.pierre@gmail.com>
2022-08-07 23:30:07 +0200

    feat: Understanding classes and their methods

---
+++ Day 16/main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+skillie = Turtle()
+print(skillie)
+skillie.shape('turtle')
+skillie.color('black', 'dark turquoise')
+skillie.forward(300)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
