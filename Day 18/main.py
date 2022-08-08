b7b0bf335b20b6a93920f87e5c13d6436d124ab9
Pierre Le Roux <leroux.pierre@gmail.com>
2022-08-08 23:07:47 +0200

    feat: Making triangles to 40 angle shapes (almost a circle)

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,36 +1,29 @@
 from turtle import Turtle, Screen
+from random import random
+
+
+def change_color():
+    R = random()
+    B = random()
+    G = random()
+
+    squirtle.color(R, G, B)
+
 
 squirtle = Turtle()
+squirtle.speed('fastest')
+squirtle.penup()
+squirtle.goto(-38, 400)
+squirtle.pendown()
 squirtle.shape('turtle')
 squirtle.color('black', 'orange')
-squirtle.home()
-squirtle.speed('fastest')
 
-for i in range(15):
-    squirtle.pendown()
-    squirtle.fd(10)
-    squirtle.penup()
-    squirtle.fd(10)
-
-
-# squirtle.penup()
-# squirtle.fd(50)
-# squirtle.dot(20, 'blue')
-# squirtle.fd(50)
-# squirtle.dot(20, 'blue')
-# squirtle.fd(50)
-# squirtle.dot(20, 'blue')
-# squirtle.fd(50)
-# squirtle.dot(20, 'blue')
-# squirtle.fd(50)
-# squirtle.dot(20, 'blue')
-# squirtle.fd(50)
-
-
-
-
-
-
+for i in range(1, 40):
+    angle = 360/(i+2)
+    change_color()
+    for j in range(i+2):
+        squirtle.fd(75)
+        squirtle.right(angle)
 
 # has to happen at the end
 screen = Screen()
