4276e795ea9c04bb4264b3eee79fffeec34c5ab5
Pierre Le Roux <leroux.pierre@gmail.com>
2022-08-08 19:04:45 +0200

    feat: Changed class to not care how it received the data

--- Day 17/question.py
+++ Day 17/question.py
@@ -1,13 +1,5 @@
-import requests
-
-
 class Question:
 
-    def __init__(self):
-        r = requests.get("https://opentdb.com/api.php?amount=1&type=boolean")
-        r = r.json()['results'][0]
-
-        self.text = r['question']\
-            .replace('&quot;', '\'')\
-            .replace('&#039;', '\'')
-        self.answer = r['correct_answer'].lower()
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
