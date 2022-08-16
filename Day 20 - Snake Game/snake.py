355d2ca65fe69ba6e533427335ef4ccb86161ab7
Pierre Le Roux <leroux.pierre@gmail.com>
2022-08-16 17:07:02 +0200

    feat: Snake class to initialise snake game

---
+++ Day 20 - Snake Game/snake.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(-40, 0), (-20, 0), (0, 0)]
+MOVE_DISTANCE = 20
+
+
+class Snake():
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for (x, y) in STARTING_POSITIONS:
+            segment = Turtle('square')
+            segment.penup()
+            segment.color('white')
+            segment.goto(x, y)
+            self.segments.append(segment)
+
+    def move(self):
+        for i in range(len(self.segments)-1):
+            self.segments[i].goto(self.segments[i+1].position())
+
+        self.segments[-1].forward(MOVE_DISTANCE)
