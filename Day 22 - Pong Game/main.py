9f622fbcb575a0d13a1afcb5cebdfdb61cdaf1df
Pierre Le Roux <leroux.pierre@gmail.com>
2022-08-22 17:30:59 +0200

    feat: Main file to run pong game

--- Day 22 - Pong Game/main.py
+++ Day 22 - Pong Game/main.py
@@ -1,14 +1,18 @@
 # from time import sleep
+from time import sleep
 from turtle import Screen
 from player import Player
+from ball import Ball
 from settings import WIDTH, HEIGHT
 
 screen = Screen()
 screen.setup(WIDTH, HEIGHT)
 screen.bgcolor('black')
+screen.tracer(0)
 
 player_1 = Player(1)
 player_2 = Player(2)
+ball = Ball()
 
 screen.listen()
 screen.onkey(key='w', fun=player_1.up)
@@ -18,10 +22,21 @@ screen.onkey(key='Down', fun=player_2.down)
 
 
 def main():
+    gaming = True
+    while gaming:
+        sleep(0.1)
+        screen.update()
+        ball.check_boarders()
+        ball.move()
 
-    # gaming = True
-    # while gaming:
-    #     sleep(0.1)
+        # ball hit by player
+        ball.update_angle(player_1.xcor(), player_1.ycor())
+        ball.update_angle(player_2.xcor(), player_2.ycor())
+
+        # ball collide with goal area behind player
+        if abs(ball.xcor()) > WIDTH/2:
+            ball.refresh()
+            main()
 
     screen.exitonclick()
 
