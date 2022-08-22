2f8a5d889e03e1a673609edcbfe5aaa5ade6795e
Pierre Le Roux <leroux.pierre@gmail.com>
2022-08-23 00:04:18 +0200

    feat: Implement new Scoreboard class

--- Day 22 - Pong Game/main.py
+++ Day 22 - Pong Game/main.py
@@ -3,6 +3,7 @@ from time import sleep
 from turtle import Screen
 from player import Player
 from ball import Ball
+from scoreboard import Scoreboard
 from settings import WIDTH, HEIGHT
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.setup(WIDTH, HEIGHT)
 screen.bgcolor('black')
 screen.tracer(0)
 
+scoreboard = Scoreboard()
 player_1 = Player(1)
 player_2 = Player(2)
 ball = Ball()
@@ -24,17 +26,21 @@ screen.onkey(key='Down', fun=player_2.down)
 def main():
     gaming = True
     while gaming:
-        sleep(0.1)
+        sleep(0.05)
         screen.update()
         ball.check_boarders()
         ball.move()
 
         # ball hit by player
-        ball.update_angle(player_1.xcor(), player_1.ycor())
-        ball.update_angle(player_2.xcor(), player_2.ycor())
+        # ic(ball.distance(player_1))
+        ball.update_angle(player_1)
+        ball.update_angle(player_2)
 
         # ball collide with goal area behind player
         if abs(ball.xcor()) > WIDTH/2:
+            scoreboard.update_score(ball.xcor() > 0)
+            player_1.refresh()
+            player_2.refresh()
             ball.refresh()
             main()
 
